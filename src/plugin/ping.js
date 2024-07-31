eaca827bca5711a2fa02b24da7252781d373a58b
Lasithruwantha <lalnishshanka3@gmail.com>
2024-07-31 18:28:14 +0530

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -4,11 +4,11 @@ const ping = async (m, sock) => {
 
   if (cmd === "ping") {
     const start = new Date().getTime();
-    await m.React('🔥');
+    await m.React('☠️');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_⃝𝙃𝘼𝙉𝙎𝘼𝙈𝘼𝙇 BOT SPEED: ${responseTime.toFixed(2)} s_*`;
+    const text = `*_⃝Lasith BOT SPEED: ${responseTime.toFixed(2)} s_*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
